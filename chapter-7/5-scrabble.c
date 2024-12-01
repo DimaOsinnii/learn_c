eff4c2ed091ed9e46ad982abbd5e5c05b0691f61
autumn_dima <ozerland@gmail.com>
2024-12-01 21:20:21 +0200

    complete forgotten task 5(scrabble) from chapter 7

---
+++ chapter-7/5-scrabble.c
@@ -0,0 +1,60 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void) {
+    printf("Enter a word: ");
+
+    char ch;
+    int value = 0;
+
+    while ((ch = getchar()) != '\n') {
+        switch (toupper(ch)) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'L':
+            case 'N':
+            case 'O':
+            case 'R':
+            case 'S':
+            case 'T':
+            case 'U':
+                value += 1;
+                break;
+            case 'D':
+            case 'G':
+                value += 2;
+                break;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                value += 3;
+                break;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                value += 4;
+                break;
+            case 'K':
+                value += 5;
+                break;
+            case 'J':
+            case 'X':
+                value += 8;
+                break;
+            case 'Q':
+            case 'Z':
+                value += 10;
+                break;
+            default:
+                return 1;
+        }
+    }
+
+    printf("Scrabble value: %d", value);
+
+    return 0;
+}
